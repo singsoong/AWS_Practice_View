aa6d1eefad822b256c5aa5e0b4c802f5ade744e3
singsoong <o__hyeah@naver.com>
2022-08-02 09:50:55 +0900

    RDS에서 데이터 가져와서 뿌리기

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+  const list = data.map((item) => <p key={item.id}>{item.name}</p>);
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/select")
+      .then(function (response) {
+        console.log(response.data);
+        setData(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{list}</h1>
     </div>
   );
 }
